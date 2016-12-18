8c9f590131eaa970d6624d16a33098d456e3b59d
Alex Nestorov <nestorov.alexander@gmail.com>
2016-12-18 23:06:12 +0200

    add implementation for methods in user services

--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -1,29 +1,65 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 
 import { User } from '../models/user';
 
 @Injectable()
 export class UserService {
+    private usersUrl = 'app/users';
+
     constructor(private http: Http) { }
 
-    getAllUsers(){
-        // TODO:
+    getAllUsers(): Promise<User[]> {
+        return this.http
+        .get(this.usersUrl)
+        .toPromise()
+        .then(response => response.json().data as User[])
+        .catch(this.handleError);
     }
 
     getUserById(id: string){
-        // TODO:
+       return this.getAllUsers()
+       .then(users => users.find(user => user.id === id));
     }
 
-    createUser(user: User){
-        // TODO:
+    createUser(user: User): Promise<User>{
+        return this.post(user);
     }
 
-    updateUser(user: User){
-        // TODO:
+    updateUser(user: User): Promise<User>{
+        return this.put(user);
     }
 
-    deleteUser(id: string){
+    //deleteUser(id: string){
         // TODO:
-    }
+    //}
+
+      private post(user: User): Promise<User> {
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http
+      .post(this.usersUrl, JSON.stringify(user), { headers: headers })
+      .toPromise()
+      .then(res => res.json().data)
+      .catch(this.handleError);
+  }
+
+   private put(user: User): Promise<User> {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    let url = `${this.usersUrl}/${user.id}`;
+
+    return this.http
+      .put(url, JSON.stringify(user), { headers: headers })
+      .toPromise()
+      .then(() => user)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any>{
+      return Promise.reject(error.message || error);
+  }
 }
