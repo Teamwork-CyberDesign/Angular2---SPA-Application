74901ef6a2ae03146a855475f5ca1be59e77012a
J0hnyBG <ivan.parvanov1@gmail.com>
2016-12-23 15:52:23 +0200

    add previous pipes to declarations

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,26 +2,42 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component'; //import home components
+import { HomeComponent } from './components/home/home.component'; // import home components
 import { LoginComponent } from './components/login/login.component'; // import login component
 import { RegisterComponent } from './components/register/register.component'; // import register component
 import { AddMarkComponent } from './components/create-mark/addMark.component'; // import component for adding marks
-import { UsersComponent} from './components/users/users.component';
-import { AboutComponent } from './components/about/about.component'; //import about component
+import { UsersComponent } from './components/users/users.component';
+import { AboutComponent } from './components/about/about.component'; // import about component
 import { routing } from './app.routing';
 import { APP_BASE_HREF } from '@angular/common';
-
-
 import { UserService } from './services/user.service';
 import { AuthenticationService } from './services/authentication.service';
+import { SortByFirstNamePipe } from './pipes/sort-by-first-name';
+import { SortByFirstThenByLastNamePipe } from './pipes/sort-by-first-then-last-name';
 
 @NgModule({
-    imports: [BrowserModule, routing, HttpModule, FormsModule], //other modules the app depends on
-    declarations: [AppComponent, AboutComponent, HomeComponent, LoginComponent, RegisterComponent, AddMarkComponent, UsersComponent], // declare all derectives and components
+    imports: [ // other modules the app depends on
+        BrowserModule,
+        routing,
+        HttpModule,
+        FormsModule],
+    declarations: [ // declare all derectives and components
+        AppComponent,
+        AboutComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        AddMarkComponent,
+        UsersComponent,
+        SortByFirstNamePipe,
+        SortByFirstThenByLastNamePipe
+    ],
     bootstrap: [AppComponent], // root component to bootstarp
-    providers: [{ provide: APP_BASE_HREF, useValue: '/' }, UserService, AuthenticationService]
+    providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        UserService,
+        AuthenticationService]
 })
 export class AppModule {
 }
