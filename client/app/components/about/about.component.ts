0a1af3b3e944f85759c4229108541cf192584ca8
Alex Nestorov <nestorov.alexander@gmail.com>
2016-12-24 23:07:51 +0200

    add simple animation in aboutComponent

--- client/app/components/about/about.component.ts
+++ client/app/components/about/about.component.ts
@@ -1,8 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,trigger, state, style, animate, transition } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-    templateUrl: 'about.component.html'
+    templateUrl: 'about.component.html',
+    animations: [
+        trigger('flyInOut', [
+            state('in', style({ opacity: 1, transform: 'translateX(0)' })),
+            transition('void => *', [
+                style({
+                    opacity: 0,
+                    transform: 'translateX(-100%)'
+                }),
+                animate('0.6s ease-in')
+            ]),
+            transition('* => void', [
+                animate('0.2s 10 ease-out', style({
+                    opacity: 0,
+                    transform: 'translateX(100%)'
+                }))
+            ])
+        ])
+    ]
 })
 export class AboutComponent implements OnInit {
     returnUrl: string;
