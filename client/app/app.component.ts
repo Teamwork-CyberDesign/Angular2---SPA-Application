4fc97f5a6828d5af4b2afa759482a76fbb878cf5
Alex Nestorov <nestorov.alexander@gmail.com>
2016-12-26 16:46:15 +0200

    add animation in navigation bar

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from './services/authentication.service';
+import { animateFactory } from 'ng2-animate';
 
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html',
+    animations:  [animateFactory(1000, 200, 'ease-in')]
 })
 export class AppComponent {
+    show: boolean = true;
     private auth: AuthenticationService;
     private notifierOptions = {
         timeOut: 3000,
