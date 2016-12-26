a140d656a5675ea979f582f7a3dc7c7164f05cb0
Alex Nestorov <nestorov.alexander@gmail.com>
2016-12-27 01:12:01 +0200

    add autocomplete for subjects and students

--- client/app/components/add-mark/add-mark.component.ts
+++ client/app/components/add-mark/add-mark.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { subjects} from '../../utils/subjects-data';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
 
 @Component({
     templateUrl: 'add-mark.component.html'
 })
 export class AddMarkComponent implements OnInit {
     returnUrl: string;
+    students: User[]
+    subjects: string[] = subjects;
+    autoCompleteModelSubjects: any;
+    autoCompleteModelStudents: User[];
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute,
+    private userService: UserService) {
     }
 
     ngOnInit() {
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+        this.loadUsers();
+    }
+
+    loadUsers() {
+        this.userService.getAllUsers()
+            .subscribe(
+                users => {
+                    this.students = users as User[];
+                    console.log(this.students);
+                },
+                err => {
+                    console.log(err);
+                });
     }
 }
