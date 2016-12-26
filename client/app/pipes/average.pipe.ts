82114e44ec7d6fb0bf23cf4684803f6f7beae889
J0hnyBG <ivan.parvanov1@gmail.com>
2016-12-26 17:12:23 +0200

    bugfix when student has no marks

--- client/app/pipes/average.pipe.ts
+++ client/app/pipes/average.pipe.ts
@@ -3,6 +3,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'average', pure: false })
 export class AveragePipe implements PipeTransform {
     transform(numbers: number[]) {
+        if (numbers.length === 0) {
+            return 0;
+        }
+
         let sum = numbers.reduce((a, b) => a + b);
         return (sum / numbers.length).toFixed(2);
     }
