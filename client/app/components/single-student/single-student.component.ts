8da40bc495024bf9cfbd61a9c0da108e645e804e
J0hnyBG <ivan.parvanov1@gmail.com>
2016-12-26 13:22:24 +0200

    added notifications to single-student

--- client/app/components/single-student/single-student.component.ts
+++ client/app/components/single-student/single-student.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Student } from '../../models/student';
+import { NotificationsService } from 'angular2-notifications';
 
 @Component({
     selector: 'single-student',
@@ -8,9 +9,11 @@ import { Student } from '../../models/student';
 
 export class SingleStudentComponent {
     @Input() data: Student;
+    private notifier: NotificationsService;
 
-    constructor() {
+    constructor(notifier: NotificationsService) {
         this.data = new Student();
+        this.notifier = notifier;
     }
 
     public get console() {
@@ -18,11 +21,13 @@ export class SingleStudentComponent {
     }
 
     private saveStudent() {
-        console.log("save");
+        console.log('TODO: save');
+        this.notifier.success('Student saved!', 'Not really');
     }
 
     private addMark(mark, subject) {
         let markInfo = this.data.marks.filter(info => info.subject === subject)[0];
         markInfo.marks.push(+mark);
+        this.notifier.success('Mark added successfully!', '');
     }
 }
