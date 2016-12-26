767159de1153474f18fbd9fec202643fe3cea42f
Alex Nestorov <nestorov.alexander@gmail.com>
2016-12-27 00:48:47 +0200

    add folder for images

--- client/app/components/home/home.component.ts
+++ client/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClassService } from '../../services/class.service';
+import { Class } from '../../models/class';
 import { animateFactory } from 'ng2-animate';
 
 @Component({
@@ -6,10 +8,27 @@ import { animateFactory } from 'ng2-animate';
     templateUrl: 'home.component.html',
     animations:  [animateFactory(1000, 200, 'ease-in')]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
     title: string;
+    classes: Class[]
 
-    constructor() {
+    constructor(private classService: ClassService) {
         this.title = 'School administrative software';
     };
+
+    ngOnInit(){
+        this.loadClasses();
+    }
+
+    loadClasses() {
+        this.classService.getClasses()
+            .subscribe(
+                cl => {
+                    this.classes = cl as Class[];
+                    console.log(this.classes);
+                },
+                err => {
+                    console.log(err);
+                });
+    }
 }
