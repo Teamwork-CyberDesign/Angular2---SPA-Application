352c2efa9cd4daa042c9269523772fa63aafa331
J0hnyBG <ivan.parvanov1@gmail.com>
2016-12-27 15:07:47 +0200

    moved teacher to class

--- client/app/components/class-list/class-list.component.ts
+++ client/app/components/class-list/class-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Class } from '../../models/class';
 import { ClassService } from '../../services/class.service';
+import { Teacher } from '../../models/teacher';
+import { AuthenticationService } from '../../services/authentication.service';
+import { TeacherService } from '../../services/teacher.service';
+import { NotificationsService } from 'angular2-notifications';
 
 @Component({
     selector: 'class-list',
@@ -9,29 +13,58 @@ import { ClassService } from '../../services/class.service';
 
 export class ClassListComponent implements OnInit {
     private classes: Class[];
+    private teacher: Teacher;
     private _currentSelection: Class;
+    private teacherService: TeacherService;
+    private auth: AuthenticationService;
+    private classService: ClassService;
+    private notifier: NotificationsService;
 
-    constructor(private data: ClassService) {
+    constructor(data: ClassService,
+                auth: AuthenticationService,
+                teacherService: TeacherService,
+                notifier: NotificationsService) {
         this.currentSelection = new Class();
+        this.classService = data;
+        this.teacher = new Teacher();
+        this.teacherService = teacherService;
+        this.auth = auth;
+        this.notifier = notifier;
     }
 
     ngOnInit() {
         this.getClasses();
+        this.getTeacher();
     }
 
     private getClasses(): void {
-        this.data.getClassesForCurrentUser()
+        this.classService.getClassesForCurrentUser()
             .subscribe(cl => {
-                if (!Array.isArray(cl) || cl.length === 0) {
-                    let blankClass = new Class();
-                    this.classes = [];
-                    this._currentSelection = blankClass;
-                } else {
-                    this.classes = cl;
-                    this._currentSelection = cl[0];
-                }
+                    if (!Array.isArray(cl) || cl.length === 0) {
+                        let blankClass = new Class();
+                        this.classes = [];
+                        this._currentSelection = blankClass;
+                    } else {
+                        this.classes = cl;
+                        this._currentSelection = cl[0];
+                    }
 
-            });
+                },
+                (err) => {
+                    this.notifier.error(err, err.message);
+                });
+    }
+
+    private getTeacher(): void {
+        let username = this.auth.getCurrentUser();
+        this.teacherService.getTeacherByUser(username)
+            .subscribe(
+                (teacher) => {
+                    this.teacher = teacher as Teacher;
+                },
+                (err) => {
+                    this.notifier.error(err, err);
+                });
     }
 
     public get currentSelection(): Class {
--- client/app/components/single-class/single-class.component.ts
+++ client/app/components/single-class/single-class.component.ts
@@ -1,47 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Class } from '../../models/class';
 import { Student } from '../../models/student';
-import { Teacher } from '../../models/teacher';
-import { AuthenticationService } from '../../services/authentication.service';
-import { TeacherService } from '../../services/teacher.service';
-import { NotificationsService } from 'angular2-notifications';
 
 @Component({
     selector: 'single-class',
     templateUrl: 'single-class.component.html'
 })
 
-export class SingleClassComponent implements OnInit {
+export class SingleClassComponent {
     @Input() data: Class;
-    private currentStudent: Student;
-    private currentTeacher: Teacher;
-    private teacherService: TeacherService;
-    private auth: AuthenticationService;
-    private notifier: NotificationsService;
+    @Input() subject: string;
 
-    constructor(teacherService: TeacherService,
-                auth: AuthenticationService,
-                notifier: NotificationsService) {
-        this.teacherService = teacherService;
-        this.notifier = notifier;
-        this.auth = auth;
+    private currentStudent: Student;
+
+    constructor() {
         this.currentStudent = new Student();
-        this.currentTeacher = new Teacher();
     }
 
     onEditClick(student: Student) {
         this.currentStudent = student;
-    }
-
-    ngOnInit() {
-        let username = this.auth.getCurrentUser();
-        this.teacherService.getTeacherByUser(username)
-            .subscribe(
-                (teacher) => {
-                    this.currentTeacher = teacher as Teacher;
-                },
-                (err) => {
-                    this.notifier.error(err, err);
-                });
     }
 }
