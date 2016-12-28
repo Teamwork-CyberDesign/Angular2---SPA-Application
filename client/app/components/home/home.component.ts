e0c0d307287d02e0e951cad32b9aceeac2c871d2
Alex Nestorov <nestorov.alexander@gmail.com>
2016-12-28 21:47:01 +0200

    fix homepage in nav-bar and return displaying classes in template

--- client/app/components/home/home.component.ts
+++ client/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Class } from '../../models';
+import { ClassService } from '../../services/class.service';
 import { animateFactory } from 'ng2-animate';
 
 @Component({
@@ -6,10 +8,28 @@ import { animateFactory } from 'ng2-animate';
     templateUrl: 'home.component.html',
     animations:  [animateFactory(1000, 200, 'ease-in')]
 })
-export class HomeComponent/* implements OnInit*/{
+export class HomeComponent implements OnInit{
     private title: string;
+    private classes: Class[];
+    private classService: ClassService;
 
-    constructor() {
+    constructor(classService: ClassService) {
         this.title = 'School administrative software';
+         this.classService = classService;
     };
+
+    ngOnInit() {
+        this.getClasses();
+    }
+
+    private getClasses(): void {
+        this.classService.getClasses()
+            .subscribe(cl => {
+                    this.classes = cl;
+
+                },
+                (err) => {
+                    console.log(err);
+                });
+    }
 }
