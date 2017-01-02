a6f4a87a381f58f79650ad8ead25f8eaa015584b
J0hnyBG <ivan.parvanov1@gmail.com>
2017-01-02 18:50:23 +0200

    removed role from registration route

--- server/routers/student-router.js
+++ server/routers/student-router.js
@@ -2,7 +2,7 @@
 module.exports = (server, studentController, isAuthenticated, user) => {
     server.get("/api/student/:username", studentController.findStudentByUsername);
     server.get("/api/student", studentController.searchStudents);
-    server.post("/api/student", isAuthenticated, user.is("teacher"), studentController.createStudent);
+    server.post("/api/student", isAuthenticated, studentController.createStudent);
     server.post("/api/student/add-class", isAuthenticated, user.is("teacher"), studentController.addStudentsToClass);
     server.put("/api/student/add-marks", isAuthenticated, user.is("teacher"), studentController.addMark);
 };
